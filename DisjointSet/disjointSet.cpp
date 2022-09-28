3ba7d936b7dcbbdcd9be4232a43b9a01ffe74426
Sourashis Mondal <sourashismondal872001@gmail.com>
2022-09-28 19:02:43 +0530

    disjoint set

---
+++ DisjointSet/disjointSet.cpp
@@ -0,0 +1,72 @@
+// Name: SOURASHIS MONDAL
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DisjointSet
+{
+    int *parent;
+    int *treeSize;
+    int n;
+
+public:
+    DisjointSet(int n)
+    {
+        parent = new int[n];
+        treeSize = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            treeSize[i] = 1;
+        }
+    }
+
+    //*returns root of the tree containing x (returns the identifier of the set containing x)
+    //*Path compression: make the root of the tree parent of all nodes in the path from root to x
+
+    int find(int x)
+    {
+        // doesnt exist
+        if (x < 0 || x >= this->n)
+            return -1;
+
+        if (x == parent[x])
+            return x;
+
+        int root = find(parent[x]);
+
+        //*for path compression: make the root of the tree parent of x
+        parent[x] = root;
+
+        return root;
+    }
+
+    void doUnion(int x, int y)
+    {
+        int root1 = find(x);
+        int root2 = find(y);
+
+        if (root1 == root2)
+            return;
+
+        if (this->treeSize[root1] <= this->treeSize[root2])
+        {
+            parent[root1] = root2;
+            this->treeSize[root2] += this->treeSize[root1];
+        }
+        else
+        {
+            parent[root2] = root1;
+            this->treeSize[root1] += this->treeSize[root2];
+        }
+    }
+};
+
+int main()
+{
+    cout << "\n\nHello earth\n\n\n";
+
+    return 0;
+}
