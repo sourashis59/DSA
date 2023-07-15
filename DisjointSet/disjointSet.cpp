2d2cb0fe789f330f215f4b82b2e2cde7e0cdb484
sourashisUbuntu <sourashiswtf@gmail.com>
2023-07-15 15:32:19 +0530

    disjoint set modification

--- DisjointSet/disjointSet.cpp
+++ DisjointSet/disjointSet.cpp
@@ -27,7 +27,7 @@ public:
     //*returns root of the tree containing x (returns the identifier of the set containing x)
     //*Path compression: make the root of the tree parent of all nodes in the path from root to x
 
-    int find(int x)
+    int findSet(int x)
     {
         // doesnt exist
         if (x < 0 || x >= this->n)
@@ -36,7 +36,7 @@ public:
         if (x == parent[x])
             return x;
 
-        int root = find(parent[x]);
+        int root = findSet(parent[x]);
 
         //*for path compression: make the root of the tree parent of x
         parent[x] = root;
@@ -44,10 +44,10 @@ public:
         return root;
     }
 
-    void doUnion(int x, int y)
+    void unionSets(int x, int y)
     {
-        int root1 = find(x);
-        int root2 = find(y);
+        int root1 = findSet(x);
+        int root2 = findSet(y);
 
         if (root1 == root2)
             return;
