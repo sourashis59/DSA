6457a0db1d63c20877736de326f2bf3b06c34a85
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-05-04 19:17:03 +0530

    Update segmentTree.cpp

--- SegmentTree/ArrayRepresentation/segmentTree.cpp
+++ SegmentTree/ArrayRepresentation/segmentTree.cpp
@@ -9,23 +9,23 @@ private:
     //arry of size 4n to store the tree
     //leftChild(i) = 2i + 1
     //rightChild(i) = 2i + 2
-    vector<long long> tree;
+    vector<long long> t;
     //size of the actual array(range)
     int n;
 
 public:
     SegmentTree(const vector<long long> &arr) {
         n = arr.size();
-        tree = vector<long long> (4 * n);
+        t = vector<long long> (4 * n);
         build(0, 0, n - 1, arr);
     }
 
-    long long rangeQuery(int qlow, int qhigh) {
-        return rangeQuery(0, 0, n - 1, qlow, qhigh);
+    long long query(int qlow, int qhigh) {
+        return query(0, 0, n - 1, qlow, qhigh);
     }
 
-    void updateInd(int ind, long long newVal) {
-        updateInd(0, 0, n - 1, ind, newVal);
+    void update(int targetArrInd, long long newVal) {
+        update(0, 0, n - 1, targetArrInd, newVal);
     }
 
     //TODO: change according to your need
@@ -38,46 +38,55 @@ private:
     int leftChild(int rootId) { return 2 * rootId + 1;}
     int rightChild (int rootId) { return 2 * rootId + 2;}
 
-    //build(rootId, low, high) = builds the segment tree rooted at rootId with range of root as [low, high]
-    void build(int rootId, int low, int high, const vector<long long> &arr) {
-        //for leaf
-        if (low == high) {
-            tree[rootId] = arr[low];
+    /*
+    [tl, th] = [low, high] range of current subtree
+    [ql, qh] = [low, high] range of query
+    tm = mid of the range of current tree
+
+    REMEMBER: To acess t[], always use node id as index (ex: t[rootId], t[leftId], t[rightId], ...)
+                never to use range (ex: tl, tm, th, ql, qh, ....) as index of t[]
+    -----------------
+    */
+
+    void build(int rootId, int tl, int th, const vector<long long> &arr) {
+        //leaf
+        if (tl == th) {
+            t[rootId] = arr[tl];
             return;
         }
-        int mid = low + (high - low) / 2;
+
+        int tm = tl + (th - tl) / 2;
         int leftId = leftChild(rootId), rightId = rightChild(rootId);
-        build(leftId, low, mid, arr);
-        build(rightId, mid + 1, high, arr);
-        tree[rootId] = combine(tree[leftId], tree[rightId]);
+        build(leftId, tl, tm, arr);
+        build(rightId, tm + 1, th, arr);
+        t[rootId] = combine(t[leftId], t[rightId]); 
     }
 
-    // no need to change qlow, qhigh in recursive calls
-    // rootId = root of the current segment tree
-    // [rlow, rhigh] = range of the root of the current tree
-    long long rangeQuery(int rootId, int rlow, int rhigh, int qlow, int qhigh) {
-        if (rlow >= qlow && rhigh <= qhigh) return tree[rootId];
+    long long query(int rootId, int tl, int th, int ql, int qh) {
+        if (tl >= ql && th <= qh) return t[rootId];
         
         int leftId = leftChild(rootId), rightId = rightChild(rootId);
-        int rmid = rlow + (rhigh - rlow) / 2;
-        if (qhigh <= rmid) return rangeQuery(leftId, rlow, rmid, qlow, qhigh);
-        if (qlow >= rmid + 1) return rangeQuery(rightId, rmid + 1, rhigh, qlow, qhigh);
-        else return combine(rangeQuery(leftId, rlow, rmid, qlow, qhigh),
-                            rangeQuery(rightId, rmid + 1, rhigh, qlow, qhigh));
+        int tm = tl + (th - tl) / 2;
+        if (qh <= tm) return query(leftId, tl, tm, ql, qh);
+        else if (ql >= tm + 1) return query(rightId, tm + 1, th, ql, qh);
+        else return combine(query(leftId, tl, tm, ql, qh), query(rightId, tm + 1, th, ql, qh));
     }
 
-    // no need to change qlow, qhigh in recursive calls
-    void updateInd(int rootId, int rlow, int rhigh, int ind, long long newVal) {
+    void update(int rootId, int tl, int th, int targetArrInd, int newVal) {
         //leaf
-        if (rlow == rhigh) {
-            tree[rootId] = newVal;
+        if (tl == th) {
+            t[rootId] = newVal;
             return;
-        }
+        } 
         int leftId = leftChild(rootId), rightId = rightChild(rootId);
-        int rmid = rlow + (rhigh - rlow) / 2;
-        if (ind <= rmid) updateInd(leftId, rlow, rmid, ind, newVal);
-        else updateInd(rightId, rmid + 1, rhigh, ind, newVal);
-        //update current range val
-        tree[rootId] = combine(tree[leftId], tree[rightId]);
+        int tm = tl + (th - tl) / 2;
+        
+        //update the child subtree
+        if (targetArrInd <= tm) update(leftId, tl, tm, targetArrInd, newVal);
+        else update(rightId, tm + 1, th, targetArrInd, newVal);
+
+        //update the val of current range
+        t[rootId] = combine(t[leftId], t[rightId]);        
     }
 };
+
