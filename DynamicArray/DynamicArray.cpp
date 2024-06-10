e9b9d4235650ba877538bbbbe3db11093788cc9c
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-06-10 22:31:35 +0530

    Update DynamicArray.cpp (bug fix)

--- DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray.cpp
@@ -1,66 +1,63 @@
-
-
 class DynamicArray {
-    int *arr;
-    int size;
-    int capacity;
+    int *_arr;
+    int _size;
+    int _capacity;
 
     static int INITIAL_CAPACITY;
-    static int GROWTH_FACTOR;
-    static int DELETION_FACTOR;
+    static double GROWTH_FACTOR;
+    static double DELETION_FACTOR;
 public:
     DynamicArray() {
-        INITIAL_CAPACITY = 8;
-        GROWTH_FACTOR = 2;
-        DELETION_FACTOR = 4;
-        
-        size = 0;
-        capacity = INITIAL_CAPACITY;
-        arr = new int[capacity];
+        _size = 0;
+        _capacity = INITIAL_CAPACITY;
+        _arr = new int[_capacity];
     }
 
-    virtual ~DynamicArray() {
-        delete []arr;
+    ~DynamicArray() {
+        delete []_arr;
     }
     
     int size() {
-        return size;
+        return _size;
     }
 
     int capacity() {
-        return capacity;
+        return _capacity;
     }
 
     int front() {
-        return arr[0];
+        return _arr[0];
     }
 
     int back() {
-        return arr[size - 1];
+        return _arr[_size - 1];
     }
 
     void resize(int newCapacity) {
         int *copy = new int[newCapacity];
-        for (int i = 0; i < size; ++i)
-            copy[i] = arr[i];
+        for (int i = 0; i < _size; ++i)
+            copy[i] = _arr[i];
         
-        delete []arr;
-        arr = copy;
-        capacity = newCapacity;
+        delete []_arr;
+        _arr = copy;
+        _capacity = newCapacity;
     }
 
     void push_back(int val) {
-        if (size == capacity) 
-            resize(capacity * GROWTH_FACTOR);
+        if (_size == _capacity) 
+            resize(_capacity * GROWTH_FACTOR);
         
-        arr[size++] = val;
+        _arr[_size++] = val;
     }
 
     void pop_back() {
-        --size;
-        if (capacity > INITIAL_CAPACITY && size * DELETION_FACTOR <= capacity)
-            resize((double)capacity * DELETION_FACTOR);
+        --_size;
+        if (_capacity > INITIAL_CAPACITY && _size * DELETION_FACTOR <= _capacity)
+            resize((double)_capacity / DELETION_FACTOR);
     }
 
 };
-
+int DynamicArray::INITIAL_CAPACITY = 8;
+double DynamicArray::GROWTH_FACTOR = 1.5;
+double DynamicArray::DELETION_FACTOR = 4;
+        
