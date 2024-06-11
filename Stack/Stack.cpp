83b00727235c893d861e1ce8a2e33089ad55140a
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-06-11 20:39:09 +0530

    Update Stack.cpp (better implementation)

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,5 +1,86 @@
 
 
+template <typename T>
+class DynamicArray {
+    T *_arr;
+    int _size;
+    int _capacity;
+
+    static int INITIAL_CAPACITY;
+    static double GROWTH_FACTOR;
+    static double DELETION_FACTOR;
+public:
+    DynamicArray() {
+        _size = 0;
+        _capacity = INITIAL_CAPACITY;
+        _arr = new T[_capacity];
+    }
+
+    ~DynamicArray() {
+        delete []_arr;
+    }
+    
+    int size() {
+        return _size;
+    }
+
+    int capacity() {
+        return _capacity;
+    }
+
+    T front() {
+        return _arr[0];
+    }
+
+    T back() {
+        return _arr[_size - 1];
+    }
+
+    void resize(int newCapacity) {
+        T *copy = new T[newCapacity];
+        for (int i = 0; i < _size; ++i)
+            copy[i] = _arr[i];
+        
+        delete []_arr;
+        _arr = copy;
+        _capacity = newCapacity;
+    }
+
+    void push_back(T val) {
+        if (_size == _capacity) 
+            resize(_capacity * GROWTH_FACTOR);
+        
+        _arr[_size++] = val;
+    }
+
+    void pop_back() {
+        --_size;
+        if (_capacity > INITIAL_CAPACITY && _size * DELETION_FACTOR <= _capacity)
+            resize((double)_capacity / DELETION_FACTOR);
+    }
+
+};
+// Define and initialize static member variables outside the class
+template <typename T>
+int DynamicArray<T>::INITIAL_CAPACITY = 8;
+
+template <typename T>
+double DynamicArray<T>::GROWTH_FACTOR = 1.5;
+
+template <typename T>
+double DynamicArray<T>::DELETION_FACTOR = 4;
+        
+
+
+
+
+
+
+
+
+
+
+
 template<typename T>
 class Stack {
 public:
@@ -58,13 +139,9 @@ public:
     bool empty() { return _size == 0; }
     
     void push(const T& val) {
-        Node *newNode = new Node(val);
         ++_size;
-        if (head == nullptr) head = newNode;
-        else {
-            newNode->next = head;
-            head = newNode;
-        }
+        Node *oldHead = head;
+        head = new Node(val, oldHead);
     }
     
     void pop() {
