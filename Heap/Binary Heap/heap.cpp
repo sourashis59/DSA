ca3825731e6196b1507f290f6b6f2b621c7cb891
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-06-20 22:19:31 +0530

    Update heap.cpp

--- Heap/Binary Heap/heap.cpp
+++ Heap/Binary Heap/heap.cpp
@@ -1,50 +1,71 @@
 #include <iostream>
 using namespace std;
 
-//min heap
-class PriorityQueue {
-private:
-    vector<long long> arr;
+// template <typename T>
+class Heap {
+    vector<int> a;
 
-    int parent(int i) { return (i - 1) / 2; }
-    int leftChild(int i) { return 2 * i + 1; }
-    int rightChild(int i) { return 2 * i + 2; }
+    int parent(int i) {return (i - 1) / 2;}
+    int left(int i) {return 2 * i + 1;}
+    int right(int i) {return 2 * i + 2;}
 
-    void swimUp(int i) {
-        while (parent(i) >= 0 && arr[i] < arr[parent(i)]) {
-            swap(arr[i], arr[parent(i)]);
+    bool less(int a, int b) {
+        return a < b;
+    }
+
+    void swim(int i) {
+        while (parent(i) >= 0 && less(a[parent(i)], a[i])) {
+            swap(a[parent(i)], a[i]);
             i = parent(i);
         }
     }
 
-    void swimDown(int i) {
-        int minInd = i;
-        if (leftChild(i) < arr.size() && arr[leftChild(i)] < arr[minInd]) 
-            minInd = leftChild(i);
-        if (rightChild(i) < arr.size() && arr[rightChild(i)] < arr[minInd]) 
-            minInd = rightChild(i);
-        
-        if (i != minInd) {
-            swap(arr[i], arr[minInd]);
-            swimDown(minInd);
-        } 
+    void sink(int i) {
+        while (left(i) < size()) {
+            int l = left(i), r = right(i);
+            int maxInd = i;
+            if (l < size() && less(a[maxInd], a[l])) maxInd = l;
+            if (r < size() && less(a[maxInd], a[r])) maxInd = r;
+            if (maxInd == i) break;
+            swap(a[maxInd], a[i]);
+            i = maxInd;
+        }
     }
+
+    // //recursive
+    // void sink(int i) {
+    //     int maxInd = i;
+    //     if (leftChild(i) < arr.size() && arr[leftChild(i)] > arr[minInd]) 
+    //         minInd = leftChild(i);
+    //     if (rightChild(i) < arr.size() && arr[rightChild(i)] > arr[minInd]) 
+    //         minInd = rightChild(i);
+        
+    //     if (i != minInd) {
+    //         swap(arr[i], arr[minInd]);
+    //         swimDown(minInd);
+    //     } 
+    // }
 
 public:
-    void push(long long x) {
-        arr.push_back(x);
-        swimUp(arr.size() - 1);
+    Heap(): a(vector<int>()) {}
+
+    void push(int val) {
+        a.push_back(val);
+        swim(a.size() - 1);
     }
 
-    long long pop() {
-        long long res = arr[0];
-        arr[0] = arr.back();
-        arr.pop_back();
-        swimDown(0);
-        return res;
+    void pop() {
+        a[0] = a.back();
+        a.pop_back();
+        sink(0);
+    }
+
+    int top() {
+        return a[0];
     }
 
     int size() {
-        return arr.size();
+        return a.size();
     }
+
 };
