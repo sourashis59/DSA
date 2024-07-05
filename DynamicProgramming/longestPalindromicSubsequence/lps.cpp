ae191fa4e70b59e283c441906ffab0884adfa34c
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-07-05 22:38:36 +0530

    Create lps.cpp

---
+++ DynamicProgramming/longestPalindromicSubsequence/lps.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    string s;
+    int n;
+
+    vector<vector<int>> memo;
+
+    // lps(i, j) = length of LPS from s[i...j]
+    int lps(int i, int j) {
+        if (i > j) return 0;
+        if (i == j) return 1;
+        
+        if (memo[i][j] != -1) return memo[i][j];
+        
+        int res = 0;
+        if (s[i] == s[j]) res = 2 + lps(i + 1, j - 1);
+        else res = max(lps(i, j - 1), lps(i + 1, j));
+        return memo[i][j] = res;
+    }
+
+    int topDown() {
+        memo = vector<vector<int>>(n, vector<int>(n, -1));
+        return lps(0, n - 1);
+    }
+
+
+    int bottomUp() {
+        vector<vector<int>> lps = vector<vector<int>>(n, vector<int>(n));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                // copy paste (except i>j condition)
+                if (i == j) {
+                    lps[i][j] = 1;
+                    continue;
+                }
+                int res = 0;
+                if (s[i] == s[j]) {
+                    res = 2;
+                    // CHANGE
+                    if (i + 1 <= j - 1) res += lps[i + 1][j - 1];
+                }
+                else res = max(lps[i][j - 1], lps[i + 1][j]);
+                lps[i][j] = res;
+            }
+        }
+        return lps[0][n - 1];
+    }
+
+
+    int longestPalindromeSubseq(string _s) {
+        s = _s;
+        n = s.size();    
+        
+        return bottomUp();
+        // return topDown();
+    }
+};
