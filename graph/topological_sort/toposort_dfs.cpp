ae80a91bafb24220cd7f1137605d869cd4beaacd
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-08-02 18:13:18 +0530

    Update toposort_dfs.cpp

--- graph/topological_sort/toposort_dfs.cpp
+++ graph/topological_sort/toposort_dfs.cpp
@@ -6,26 +6,24 @@ public:
 	void dfs_topo_sort(int u, vector<bool> &visited, stack<int> &reversePostorder) {
 	    visited[u] = true;
 	    for (int v: adj[u]) {
-	        if (!visited[v])
-	            dfs_topo_sort(v, visited, res);
-	    }
+		if (!visited[v]) dfs_topo_sort(v, visited, res);
 	    reversePostorder.push(u);
 	}
 	 
-	vector<int> topoSort(int n, vector<vector<int>>& _adj[])	{
+	vector<int> topoSort(int n, vector<vector<int>>& _adj) {
 	    adj = _adj;
 	    vector<bool> visited(n, false);
 	    stack<int> reversePostorder;
 	    
 	    for (int u = 0; u < n; ++u) {
-	        if (!visited[u]) 
-	            dfs_topo_sort(u, visited, reversePostorder);
+		if (!visited[u]) 
+		    dfs_topo_sort(u, visited, reversePostorder);
 	    }
 	    
 	    vector<int> res;
 	    while (res.size()) {
-	        res.push_back(reversePostorder.top());
-	        reversePostorder.pop();
+		res.push_back(reversePostorder.top());
+		reversePostorder.pop();
 	    }
 	    return res;
 	}
