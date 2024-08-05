f82aa88ab3782199e444104d6816e092c98d336e
Sourashis Mondal <74865683+sourashis59@users.noreply.github.com>
2024-08-05 08:43:57 +0530

    Update lcs_of_3_strings.cpp

--- DynamicProgramming/lcs/lcs_of_3_strings.cpp
+++ DynamicProgramming/lcs/lcs_of_3_strings.cpp
@@ -35,7 +35,37 @@ public:
         }
         return l[0][0][0];
     }
-    
+
+    int bottomUpSpaceOptimized() {
+        // Rule: Replace each l[i] with l and l[i+1] with lprev and shift pointers after i's loop
+        // l -> l[i]
+        // lprev -> l[i+1]
+        vector<vector<int>> l(n2 + 1, vector<int>(n3 + 1));
+        vector<vector<int>> lprev(n2 + 1, vector<int>(n3 + 1));
+        for (int i = n1; i >= 0; --i) {
+            for (int j = n2; j >= 0; --j) {
+                for (int k = n3; k >= 0; --k) {
+                    if (i == n1 || j == n2 || k == n3) {
+                        l[j][k] = 0;
+                        continue;
+                    }    
+                    
+                    int res = 0;
+                    if (s1[i] == s2[j] && s2[j] == s3[k]) res = 1 + lprev[j + 1][k + 1];
+                    else res = max({
+                                    l[j][k + 1],
+                                    l[j + 1][k],
+                                    lprev[j][k],
+                                });
+                    
+                    l[j][k] = res;
+                }
+            }
+            // shift pointers
+            lprev = l;
+        }
+        return l[0][0];
+    }
     
     int LCSof3 (string A, string B, string C, int n1, int n2, int n3) {
         s1 = A, s2 = B, s3 = C;
